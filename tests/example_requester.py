217026b7460d6f2996f05dc19fe5dd01ed15096f
Jack O'Quin <jack.oquin@gmail.com>
2013-12-14 10:31:01 -0600

    make example_requester match the doc version

--- tests/example_requester.py
+++ tests/example_requester.py
@@ -5,8 +5,8 @@
 import rospy
 from scheduler_msgs.msg import Request
 from scheduler_msgs.msg import Resource
+from rocon_scheduler_requests import TransitionError
 from rocon_scheduler_requests.requester import Requester
-from rocon_scheduler_requests.transitions import TransitionError
 
 def feedback(rset):
     """ Scheduler feedback function. """
