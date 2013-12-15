886c022dd880fc996c21c3702ddd1a2163369daf
Jack O'Quin <jack.oquin@gmail.com>
2013-12-15 13:00:19 -0600

    convert example requester to a simpler class implementation (#16)

--- tests/example_requester.py
+++ tests/example_requester.py
@@ -1,48 +1,44 @@
 #!/usr/bin/env python
-
 """ Requester usage example. """
-
 import rospy
-from scheduler_msgs.msg import Request
-from scheduler_msgs.msg import Resource
-from rocon_scheduler_requests import TransitionError
+from scheduler_msgs.msg import Request, Resource
 from rocon_scheduler_requests.requester import Requester
 
 
-def feedback(rset):
-    """ Scheduler feedback function. """
-    for rq in rset.values():
-        if rq.msg.status == Request.WAITING:
-            rospy.loginfo('Request queued: ' + str(rq.get_uuid()))
-        elif rq.msg.status == Request.GRANTED:
-            rospy.loginfo('Request granted: ' + str(rq.get_uuid()))
-        elif rq.msg.status == Request.RELEASED:
-            rospy.loginfo('Request canceled: ' + str(rq.get_uuid()))
+class ExampleRequester:
 
-if __name__ == '__main__':
-    rospy.init_node("example_requester")
-    rqr = Requester(feedback)
+    def __init__(self):
+        rospy.init_node("example_requester")
+        self.rqr = Requester(self.feedback)
+        self.request_turtlebot()
+        self.timer = rospy.Timer(rospy.Duration(2.0), self.periodic_update)
+        rospy.spin()
 
-    rospy.loginfo('requesting any tutlebot able to run example_rapp')
-    resource = Resource(name='example_rapp',
-                        platform_info='linux.*.ros.turtlebot.*')
-    request_id = rqr.new_request([resource])
-    rqr.send_requests()
+    def feedback(self, rset):
+        """ Scheduler feedback function. """
+        for rq in rset.values():
+            if rq.msg.status == Request.WAITING:
+                rospy.loginfo('Request queued: ' + str(rq.get_uuid()))
+            elif rq.msg.status == Request.GRANTED:
+                rospy.loginfo('Request granted: ' + str(rq.get_uuid()))
+            elif rq.msg.status == Request.RELEASED:
+                rospy.loginfo('Request canceled: ' + str(rq.get_uuid()))
 
-    # Loop once every two seconds in the main thread.
-    cycle = rospy.Rate(0.5)
-    while not rospy.is_shutdown():
-        cycle.sleep()
+    def periodic_update(self, event):
+        """ Timer event handler for periodic request updates. """
         try:
             # cancel the previously-issued request
-            rqr.rset[request_id].cancel()
+            self.rqr.rset[self.request_id].cancel()
         except KeyError:
-            rospy.loginfo('old request no longer exists: '
-                          + str(request_id))
-            rospy.loginfo('requesting another similar robot now')
-            request_id = rqr.new_request([resource])
-            rqr.send_requests()
-        except TransitionError:
-            rospy.loginfo('request not releasable: ' + str(request_id))
-            rqr.cancel_all()
-            rqr.send_requests()
+            # previous request is gone, request another similar robot
+            self.request_turtlebot()
+
+    def request_turtlebot(self):
+        """ Request any tutlebot able to run *example_rapp*. """
+        bot = Resource(name='example_rapp',
+                       platform_info='*.*.ros.turtlebot.*')
+        self.request_id = self.rqr.new_request([bot])
+        self.rqr.send_requests()
+
+if __name__ == '__main__':
+    node = ExampleRequester()
