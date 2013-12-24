5d466a7236f836fe6396d41579ed5dfaa459968e
Jack O'Quin <jack.oquin@gmail.com>
2013-12-23 20:14:18 -0600

    create disconnect and reconnect test case (#18)
    not working as expected yet, needs special reconnection merge()

---
+++ tests/timeout_reconnect.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+""" Requester for testing scheduler timeout and reconnect. """
+
+# enable some python3 compatibility options:
+from __future__ import absolute_import, print_function, unicode_literals
+
+import unittest
+import rospy
+from scheduler_msgs.msg import Resource
+from rocon_scheduler_requests.transitions import Request
+from rocon_scheduler_requests import Requester
+
+
+class TestTimeoutRequester(unittest.TestCase):
+
+    def test_example_requester_(self):
+        """ Initialize ROCON scheduler node for example requester. """
+        rospy.init_node("test_example_scheduler")
+        self.rqr = Requester(self.feedback, frequency=1.0)
+        self.next_step = self.step1     # first step of test sequence
+        self.timer = rospy.Timer(rospy.Duration(2.0), self.periodic_update)
+        rospy.spin()
+
+    def feedback(self, rset):
+        """ Scheduler feedback function. """
+        rospy.loginfo('feedback callback:')
+        for rq in rset.values():
+            rospy.logdebug('  ' + str(rq))
+            if rq.msg.status == Request.WAITING:
+                rospy.loginfo('  request queued: ' + str(rq.get_uuid()))
+            elif rq.msg.status == Request.GRANTED:
+                rospy.loginfo('  request granted: ' + str(rq.get_uuid()))
+            elif rq.msg.status == Request.CLOSED:
+                rospy.loginfo('  request closed: ' + str(rq.get_uuid()))
+            elif rq.msg.status == Request.PREEMPTING:
+                rospy.loginfo('  request preempted (reason='
+                              + str(rq.msg.reason) + '): '
+                              + str(rq.get_uuid()))
+                rq.cancel()     # release preempted resources immediately
+
+    def periodic_update(self, event):
+        """ Timer event handler for periodic request updates.
+
+        Invokes self.next_step(), unless ``None``.
+        """
+        if self.next_step is not None:  # more to do?
+            self.next_step()
+        else:                           # no more steps
+            rospy.signal_shutdown('test completed.')
+
+    def request_turtlebot(self):
+        """ Request any tutlebot able to run *example_rapp*.
+
+        :returns: UUID of new request sent.
+        """
+        bot = Resource(name='example_rapp',
+                       platform_info='*.*.ros.turtlebot.*')
+        rq_id = self.rqr.new_request([bot])
+        rospy.loginfo('  new request: ' + str(rq_id))
+        return rq_id
+
+    def verify(self, rq_list):
+        self.assertEqual(len(self.rqr.rset), len(rq_list))
+        for rq in rq_list:
+            self.assertTrue(rq in self.rqr.rset)
+
+    def step1(self):
+        rospy.loginfo('Step 1')
+        # allocate two requests and send them immediately
+        self.rq1 = self.request_turtlebot()
+        self.rq2 = self.request_turtlebot()
+        self.verify([self.rq1, self.rq2])
+        self.rqr.send_requests()
+        self.next_step = self.step2
+
+    def step2(self):
+        rospy.loginfo('Step 2')
+        self.verify([self.rq1, self.rq2])
+
+        # send another request, which should wait
+        self.rq3 = self.request_turtlebot()
+        self.verify([self.rq1, self.rq2, self.rq3])
+
+        self.rqr.send_requests()
+        self.next_step = self.step3
+
+    def step3(self):
+        rospy.loginfo('Step 3')
+        self.verify([self.rq1, self.rq2, self.rq3])
+        self.rqr.rset[self.rq2].cancel()
+        self.rq4 = self.request_turtlebot()
+        self.verify([self.rq1, self.rq2, self.rq3, self.rq4])
+        self.rqr.send_requests()
+        self.next_step = self.step4
+
+    def step4(self):
+        rospy.loginfo('Step 4')
+        self.verify([self.rq1, self.rq3, self.rq4])
+
+        # save requester ID and request set
+        self.requester_id = self.rqr.requester_id
+        self.rset = self.rqr.rset
+        del self.rqr                    # disconnect requester
+        self.wait_cycles = 4            # cycles to wait
+        self.next_step = self.step5
+
+    def step5(self):
+        rospy.loginfo('Step 5.' + str(5 - self.wait_cycles))
+        self.wait_cycles -= 1
+        if self.wait_cycles > 0:        # wait some more?
+            return
+
+        # reconnect requester using the same UUID
+        rospy.loginfo('reconnect requester')
+        self.rqr = Requester(self.feedback, frequency=1.0,
+                             uuid=self.requester_id)
+        self.rqr.rset = self.rset       # restore former request set
+        self.rq5 = self.request_turtlebot() # make a new request
+        self.next_step = self.step6
+
+    def step6(self):
+        rospy.loginfo('Step 6')
+        self.verify([self.rq5])
+        self.next_step = None           # done
+
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun('rocon_scheduler_requests',
+                   'timeout_requester', TestTimeoutRequester)
